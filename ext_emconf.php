eee98368d0e35be28e89e90cb24c0b6e744e97c9
nitsan-technologies <navdeep.nitsan@gmail.com>
2023-12-12 11:26:32 +0530

    [INIT] Initial upload with v1.0.0

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF['ns_headless_blog'] = [
+    'title' => 'Ns Headless Blog',
+    'description' => 'This extension provides integration with blog to output content from TYPO3 in JSON format.',
+    'category' => 'plugin',
+    'author' => '',
+    'author_email' => '',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.5.99',
+            'frontend' => '12.0.0-12.5.99',
+            'blog' => '12.0.0-12.5.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [
+            'headless' => '2.0.0-4.9.9',
+        ],
+    ],
+];
